ab3cd3c4f450f27d46ec734fcea6e114cae3ef08
Webber <sdwh@mail.john.doe.com>
2022-02-01 19:44:57 +0800

    layout fit for iPad

--- src/components/LeaderPokeBoxes.vue
+++ src/components/LeaderPokeBoxes.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container">
+  <div class="container-fluid container-lg">
       <template v-for="(leader, index) in leadersData" :key="leader">
-      <div class="row mb-5">
+      <div class="row mb-5 align-items-center p-3">
           <div class="col-md-4" :class="{ 'order-md-1': index % 2 === 1}">
               <img alt="Gym Leader" :src="require('../assets/' + leader['leader-name'] + '.png')" class="gym-leader">
               <h2 class="gym-leader-name">{{ leader['leader-name'] }}</h2>
@@ -9,7 +9,7 @@
           <div class="col-11 col-md-8 border shadow pb-5 m-auto rounded-3">
               <div class="gym-name">{{ leader['gym-name'] }}</div>
               <div class="row h-100">
-                  <div class="col-6 col-md-4" v-for="poke in leader['pokemons']" :key="poke">
+                  <div class="col-6 col-lg-4" v-for="poke in leader['pokemons']" :key="poke">
                       <img :src="require('../assets/' + poke.name + '.png')" class="img-fluid">
                       <div class="poke-name">{{ poke.name }}</div>
                       <div>Lv.{{ poke.lv }}</div>
